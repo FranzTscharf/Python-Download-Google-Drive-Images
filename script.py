4290b67224e5124258e29cd9821bfcec4e24c1e6
Viktor Walter-Tscharf <33022356+FranzTscharf@users.noreply.github.com>
2021-01-28 21:17:48 -0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,25 @@
+import urllib.request
+import csv
+import time
+import gdown
+
+def get_images():
+	with open('/Users/viktorwaltertscharf/Desktop/space_gallery_202101281940.csv', "rt", encoding='utf-8') as infile:
+		read = csv.reader(infile, delimiter=",")
+		for row in read :
+			print (row[1])
+			if len(row) >= 2:
+				words = row[1].split("=");
+				if len(words) == 3:
+					print (words[2])
+					#time.sleep(5)
+					try:
+						#urllib.request.urlretrieve(str(row[1]), str(words[2]))
+						gdown.download(str(row[1]), str(words[2]), quiet=False)
+					except:
+						print("errorrr")
+			else:
+				print ("nothing")
+	print("done")
+
+get_images()
